a80d5f866146dee423dc60b5fbc637e42d604036
ziad amer <ziadamer112233@gmail.com>
2023-01-17 21:07:52 +0200

    by mohamed

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagram-api-env.eba-pgmwszpb.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 
